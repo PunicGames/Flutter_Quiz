00cc3bb6c38545f4ddd59e3ddf81a57a7417e6cf
Alvaroolav3D <alvaroolavD2000@gmail.com>
2022-11-25 01:24:17 +0100

    navigation bar
    navigation bar que cambia entre difrentes paginas

---
+++ flutter_quiz/lib/page/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/page/home_page.dart';
+import 'package:flutter_quiz/page/settings_page.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+class MainPage extends StatefulWidget {
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final navigationKey = GlobalKey<CurvedNavigationBarState>();
+  int index = 0;
+
+  final screens = [
+    HomePage(),
+    SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(Icons.home, size: 30),
+      Icon(Icons.settings, size: 30),
+    ];
+
+    return Scaffold(
+      //extendBody: true,
+      body: screens[index],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          iconTheme: IconThemeData(color: Colors.red),
+        ),
+        child: CurvedNavigationBar(
+          key: navigationKey,
+          color: Colors.blue,
+          buttonBackgroundColor: Colors.green,
+          backgroundColor: Colors.transparent,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          height: 60,
+          index: index,
+          items: items,
+          onTap: (index) => setState(() => this.index = index),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_quiz/lib/page/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Text("settings"),
+        ),
+      );
+}
